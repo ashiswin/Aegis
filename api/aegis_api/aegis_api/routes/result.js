e52b646e489facadabd9a0d902fa558b279498fa
justiniansiah <justinian_siah@mymail.sutd.edu.sg>
2018-02-25 07:09:59 +0800

    removed api folder to prevent conflicts

--- api/aegis_api/aegis_api/routes/result.js
+++
@@ -1,9 +0,0 @@
-﻿var express = require('express');
-var router = express.Router();
-var resultController = require('../controllers/resultController');
-
-router.get('/', resultController.read_result_from_id);
-router.get('/user', resultController.read_result_by_user);
-router.get('/create', resultController.create_result);
-
-module.exports = router;
--- api/aegis_api/aegis_api/routes/user.js
+++
@@ -1,7 +0,0 @@
-﻿var express = require('express');
-var router = express.Router();
-var userController = require('../controllers/userController');
-
-router.get('/', userController.read_user_from_nric);
-
-module.exports = router;
